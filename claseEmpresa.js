a68adf729de840dd0b75492e5a8d3c6e650c1011
FlorenciaVillar0411 <93131066+FlorenciaVillar0411@users.noreply.github.com>
2021-10-27 08:47:21 -0300

    Add files via upload

---
+++ claseEmpresa.js
@@ -0,0 +1,11 @@
+class empresa {
+    constructor(pCedula, pRazonSocial, pNombreFantasia, pNombreUsuario, pContrasenia) {
+        this.rut = pCedula;
+        this.razonSocial = pRazonSocial;
+        this.nombreFantasia = pNombreFantasia;
+        this.nombreUsuario = pNombreUsuario;
+        this.contrasenia = pContrasenia;
+        this.vehiculo = pVehiculo;
+        this.habilitado = false
+    }
+}
