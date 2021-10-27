134c37aeef890b1d7f134085cfde2eb606118c5b
FlorenciaVillar0411 <93131066+FlorenciaVillar0411@users.noreply.github.com>
2021-10-27 09:36:00 -0300

    Delete clasePersona.js

--- clasePersona.js
+++
@@ -1,9 +0,0 @@
-class persona {
-    constructor(pCedula, pNombre, pApellido, pNombreUsuario, pContrasenia) {
-        this.cedula = pCedula;
-        this.nombre = pNombre;
-        this.apellido = pApellido;
-        this.nombreUsuario = pNombreUsuario;
-        this.contrasenia = pContrasenia;
-    }
-}
